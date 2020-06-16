51e297ec9dd73c7b9f52af9df178fe0ba2435f6c
rolinmb <66855226+rolinmb@users.noreply.github.com>
2020-06-15 20:57:26 -0500

    Created stock_chart.py
    new file

---
+++ stock_chart.py
@@ -0,0 +1,8 @@
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+if __name__ == '__main__':
+	# Anyone can use this key. AlphaVantage gives them out for free w/ no limit
+	key = 'K2CQV5DF42ONO1EY' 
