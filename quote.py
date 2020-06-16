d7b5dcc548227ed373541cad136eb261ee052cb2
rolinmb <66855226+rolinmb@users.noreply.github.com>
2020-06-15 19:53:28 -0500

    Updated quote.py
    added functions and ticker test

--- quote.py
+++ quote.py
@@ -4,5 +4,25 @@ import time
 import sys
 import re
 
+def stripCommas(s):
+	return re.sub(',(?!\s+\d$)','',s)
+	
+def getPrice(html):
+	soup = BeautifulSoup(html,'html.parser')
+	val = soup.find_all('p',class_='data bgLast')
+	content = [c.text for c in val]
+	return float(stripCommas(''.join(content)))
+	
+def getQuote(url):
+	resp = requests.get(url)
+	html = resp.text
+	price = getPrice(html)
+	print('\t Current Price: $',price)
+
 if __name__ == '__main__':
-	print('test')
+	try:
+		ticker = sys.argv[1].lower()
+	except:
+		sys.exit('No ticker/symbol entered.')
+	
+	print(ticker)
